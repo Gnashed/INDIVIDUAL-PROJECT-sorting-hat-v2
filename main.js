84d2d34dfadd7eeba327ab1febf617097e6a2fa4
Tion Blackmon <tion.blackmon@outlook.com>
2024-07-27 12:36:59 -0500

    Created some sample data for sorted students and expelled students.

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 import { renderToDom } from "./utils/renderToDom.js";
+import { student, expelled_student } from "./data/sampleData.js";
 
 console.log('JS linked');
+console.log(student);
+console.log(expelled_student);
 // renderToDom('#app', '<h1>JavaScript is connected!</h1>');
