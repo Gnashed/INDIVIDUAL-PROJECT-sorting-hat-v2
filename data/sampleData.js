2ac688f1ae9441fdbf37bf2ebb3a132f3a8c878e
Tion Blackmon <tion.blackmon@outlook.com>
2024-07-27 13:00:34 -0500

    Got data from student array to render to a card in the First Year's section.

--- data/sampleData.js
+++ data/sampleData.js
@@ -3,30 +3,35 @@ const student = [
     id: 1,
     name: 'Adianna Foster',
     house: 'Ravenclaw',
+    imageUrl: '',
     isExpelled: false
   },
   {
     id: 2,
     name: 'Victor Cho',
     house: 'Hufflepuff',
+    imageUrl: '',
     isExpelled: false
   },
   {
     id: 3,
     name: 'Gordon Gecko',
     house: 'Slytherin',
+    imageUrl: '',
     isExpelled: true
   },
   {
     id: 4,
     name: 'Jacob Wisely',
     house: 'Gryffindor',
+    imageUrl: '',
     isExpelled: false
   },
   {
     id: 5,
     name: 'Alisa Wisely',
     house: 'Slytherin',
+    imageUrl: '',
     isExpelled: false
   },
 ];
