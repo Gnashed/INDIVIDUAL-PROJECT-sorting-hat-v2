a98d4d48bcb63d8cd99ee715b5eaf313ba157c50
Tion Blackmon <tion.blackmon@outlook.com>
2024-07-27 13:12:03 -0500

    Rendering the house name and an image on each card.

--- data/sampleData.js
+++ data/sampleData.js
@@ -3,35 +3,35 @@ const student = [
     id: 1,
     name: 'Adianna Foster',
     house: 'Ravenclaw',
-    imageUrl: '',
+    imageUrl: 'https://loremflickr.com/320/200/girl-from-harry-potter',
     isExpelled: false
   },
   {
     id: 2,
     name: 'Victor Cho',
     house: 'Hufflepuff',
-    imageUrl: '',
+    imageUrl: 'https://loremflickr.com/320/200/girl-from-harry-potter',
     isExpelled: false
   },
   {
     id: 3,
     name: 'Gordon Gecko',
     house: 'Slytherin',
-    imageUrl: '',
+    imageUrl: 'https://loremflickr.com/320/200/girl-from-harry-potter',
     isExpelled: true
   },
   {
     id: 4,
     name: 'Jacob Wisely',
     house: 'Gryffindor',
-    imageUrl: '',
+    imageUrl: 'https://loremflickr.com/320/200/girl-from-harry-potter',
     isExpelled: false
   },
   {
     id: 5,
     name: 'Alisa Wisely',
     house: 'Slytherin',
-    imageUrl: '',
+    imageUrl: 'https://loremflickr.com/320/200/girl-from-harry-potter',
     isExpelled: false
   },
 ];
